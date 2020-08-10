a22c513036f54e6e55c7dbb413391cb336febc81
agbemazior <agbemazior@gmail.com>
2020-08-10 17:59:35 +0000

    Assignment 3

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './css/layout.css'
+
+function Navbar() {
+    return (
+        <di>
+            <nav className="navbar">
+            <div className="container">
+            <a href="#">Hulk</a>
+            <a href="#" class="logo is-active">Marvel</a>
+            <a href="#">Venom</a>
+            </div>
+            </nav>
+        </di>
+    )
+}
+
+export default Navbar;
